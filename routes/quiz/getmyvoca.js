385f27b5e765e5fcab71c733c4f285ad5c150967
leejisoo <ljsgold2001@naver.com>
2020-06-22 18:40:15 +0900

    [MOD] MYVOCA 데이터수정

--- routes/quiz/getmyvoca.js
+++ routes/quiz/getmyvoca.js
@@ -15,16 +15,16 @@ router.get('/:category_name', authUtil.isLoggedin, async(req, res)=>{
     const useridx = req.decoded.idx;
     console.log(useridx)
     console.log(category_name)
-
-    const getMyVocaQuery = 'SELECT  * FROM myvoca WHERE userIdx =? and category_name =?';
+    //나의 단어장 + 단어장에서 단어목록을 전송해줄때 사용하는 쿼리
+    const getMyVocaQuery = 'SELECT * FROM myvoca natural join voca where userIdx = ? AND category_name = ?;';
     const getMyVocaResult = await db.queryParam_Arr(getMyVocaQuery , [useridx,category_name]);
-
+    // 카테고리 Name이 올바른지 알아보는 쿼리
     const checkCategoryQuery = 'SELECT * FROM category WHERE category_name =?'
     const checkCategoryResult = await db.queryParam_Arr(checkCategoryQuery , [category_name]);
     console.log(checkCategoryResult)
 
 
-    if(checkCategoryResult.length == 0){
+    if(checkCategoryResult.length == 0){//카테고리 이름이 올바르지않으면 오류전송
         res.status(200).send(util.successFalse(statusCode.BAD_REQUEST,resMessage.WRONG_CATEGORY_NAME));
     }else{
         if(getMyVocaResult.length == 0){//WORD GET 실패시
